3d3f8bc4863c925ee6c5dae6458b2de37816bf3f
iloneer <iloneercom@gmail.com>
2022-07-13 20:35:34 +0900

    tmp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,44 @@
 import { Global, css } from "@emotion/react";
 import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import "./reset.css";
 
 const globalStyle = css`
   body {
     color: #333;
+    width: 100%;
+    height: 100%;
+    margin-top: 1rem;
+    background-image: url("/background.webp");
+    background-repeat: repeat;
+  }
+
+  p {
+    width: 70%;
+    font-family: "Hannari Mincho";
+    font-size: 0.75rem;
+    letter-spacing: 0.1rem;
+    margin: auto;
+  }
+
+  .selector {
+    font-family: "Luscious Script";
+    font-size: 2rem;
+    letter-spacing: 0.2rem;
+  }
+
+  .destructive {
+    color: #ff4500;
   }
 `;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Global styles={globalStyle} />
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Global styles={globalStyle} />
+        <Component {...pageProps} />
+      </RecoilRoot>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
+import { Header } from "features/Header";
+import { Description } from "features/Description";
+import { ImageSelector } from "features/ImageSelector";
+import { PreviewEditor } from "features/PreviewEditor";
+import { usePreviewState } from "hooks/usePreviewState";
+import { ImagePreview } from "features/ImagePreview";
 import { css } from "@emotion/react";
+import { Footer } from "features/Footer";
+import { Artifact } from "features/Artifact";
 
-const headerStyle = css`
-  color: gray;
+const mainStyle = css`
+  width: 100%;
+  height: 100%;
 `;
 
-const IndexPage = () => <h1 css={headerStyle}>Hello</h1>;
+const IndexPage = () => {
+  const { preview } = usePreviewState();
+  const { originImgPath } = preview;
+  return (
+    <main css={mainStyle}>
+      <Header />
+      <Description />
+      {originImgPath ? <ImagePreview /> : <ImageSelector />}
+      <PreviewEditor />
+      <Footer />
+      <Artifact />
+    </main>
+  );
+};
 
 export default IndexPage;
